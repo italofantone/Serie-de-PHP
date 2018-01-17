ebc401b71ce6aec8a33047535dd0cdd19b613b0b
Italo Morales F <i@italomoralesf.com>
2018-01-17 14:44:06 -0500

    include

---
+++ include/greet.php
@@ -0,0 +1,5 @@
+<?php
+
+	function greet($name, $message) {
+        return "{$name}, {$message}!";
+    }
---
+++ include/index.php
@@ -0,0 +1,5 @@
+<?php
+
+	include 'greet.php';
+
+    echo greet('Italo Morales', 'Buenas tardes');
